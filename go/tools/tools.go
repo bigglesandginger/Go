0431a2d43492b6d5a252755822d695b29466b472
bigglesandginger <bigglesandginger@gmail.com>
2016-06-01 21:35:30 -0400

    working keys

--- go/tools/tools.go
+++ go/tools/tools.go
@@ -1,11 +1,25 @@
-package tools
+package main
 
 import (
   "github.com/bigglesandginger/go/utils"
   "fmt"
+  "flag"
+)
+
+var (
+  sshKey = flag.String("k", "", "location of SSH key")
+  sshServer = flag.String("h", "", "server to SSH to")
+  user = flag.String("u", "", "username")
+  pwd = flag.String("p", "", "password")
 )
 
 func main() {
-  fmt.Println("Hello World")
-  utils.Parse()
+  flag.Parse()
+
+  if len(*sshKey) == 0 || len(*sshServer) == 0 || len(*user) == 0 || len(*pwd) == 0 {
+    flag.PrintDefaults()
+  } else {
+    output := utils.SshToServer(*user, *pwd, *sshKey, *sshServer)
+    fmt.Println(output)
+  }
 }
--- go/utils/utils.go
+++ go/utils/utils.go
@@ -1,16 +1,61 @@
 package utils
 
 import (
-  "flag"
+  "crypto/x509"
+  "encoding/pem"
+  "golang.org/x/crypto/ssh"
+  //"io"
+  "io/ioutil"
+  //"os"
+  "log"
+  "bytes"
 )
 
-func Parse() {
-  var sshKey string
-
-  flag.StringVar(&sshKey, "k", "", "location of SSH key")
-  flag.Parse()
-
-  if len(sshKey) == 0 {
-    flag.PrintDefaults()
+func SshToServer(user string, pwd string, key string, server string) string {
+  pemKey, err := ioutil.ReadFile(key)
+  if err != nil {
+      log.Fatalf("unable to read private key: %v", err)
   }
+
+  block, _ := pem.Decode([]byte(pemKey))
+
+  derKey, err := x509.DecryptPEMBlock(block, []byte(pwd))
+  if err != nil {
+      log.Fatalf("unable to decrypt private key: %v", err)
+  }
+
+  privKey, err := x509.ParsePKCS1PrivateKey(derKey)
+  if err != nil {
+      log.Fatalf("unable to decrypt pkcs1 private key: %v", err)
+  }
+
+  signer, err := ssh.NewSignerFromKey(privKey)
+  if err != nil {
+      log.Fatalf("unable to parse private key: %v", err)
+  }
+
+  config := &ssh.ClientConfig{
+  	User: user,
+  	Auth: []ssh.AuthMethod{
+      ssh.PublicKeys(signer),
+    },
+  }
+
+  conn, err := ssh.Dial("tcp", server, config)
+  if err != nil {
+    log.Fatalf("unable to connect: %s", err)
+  }
+  defer conn.Close()
+
+  session, err := conn.NewSession()
+  if err != nil {
+    log.Fatalf("unable to create session: %s", err)
+  }
+  defer session.Close()
+
+  var stdoutBuf bytes.Buffer
+  session.Stdout = &stdoutBuf
+  session.Run("ls -l")
+
+  return stdoutBuf.String()
 }
